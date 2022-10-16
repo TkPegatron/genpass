accc9b68ea0b161784d4be74c64fcfd66ed04ff1
Elliana L. Perry <elliana.perry@gmail.com>
2022-10-16 06:01:42 -0400

    cleaned up arguments

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,14 @@ pub const NUMBERS: &str = "0123456789";
 pub const SYMBOLS: &str = ")(*&^%$#@!~";
 
 #[derive(Parser, Debug)]
-#[command(author = "Elliana Perry <elliana.perry@gmail.com>")]
+#[command(author, version, about)]
 pub struct PasswordConfig {
-    #[arg(long = "pl", default_value = "2", help="Number of words in passphrase section")]
-    passphrase_length: usize,
-    #[arg(long = "kll", default_value = "4", help="Length of letters in key section")]
-    key_let_length: u32,
-    #[arg(long = "knl", default_value = "5", help="Length of numbers in key section")]
-    key_num_length: u32,
-    #[arg(short = 'P', long = "passphrase-seperator", default_value = ":", help="Character to seperate the words of the phrase")]
-    passphrase_seperator: String,
-    #[arg(short = 'K', long = "key-seperator", default_value = "@", help="Character to seperate the key from the phrase")]
-    key_seperator: String,
-    #[arg(short = 'n', long = "number", default_value = "1", help = "Number of passwords to generate")]
-    number_of_passwords: u32
+    #[arg(long, default_value = "2", help="Number of words in passphrase section")] passphrase_length: usize,
+    #[arg(long, default_value = "4", help="Length of letters in key section")] key_let_length: u32,
+    #[arg(long, default_value = "5", help="Length of numbers in key section")] key_num_length: u32,
+    #[arg(long, default_value = ":", help="Character to seperate the words of the phrase")] passphrase_seperator: String,
+    #[arg(long, default_value = "@", help="Character to seperate the key from the phrase")] key_seperator: String,
+    #[arg(long, default_value = "1", help = "Number of passwords to generate")] number_of_passwords: u32
 }
 
 fn main() {
